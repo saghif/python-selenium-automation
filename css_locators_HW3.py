a78c567e3eeabc9d0ecb1c6405016c56c7a9c7b1
saghif <saghi.farhandi@gmail.com>
2022-05-13 20:59:54 -0700

    adding homeworks1

---
+++ css_locators_HW3.py
@@ -0,0 +1,34 @@
+"""
+Amazon Logo:
+driver.find_element(By.CSS_SELECTOR, 'i[aria-label="Amazon"]')
+
+Create Account field:
+driver.find_element(By.CSS_SELECTOR,"h1.a-spacing-small")
+
+Your name:
+driver.find_element(By.CSS_SELECTOR, '#ap_customer_name')
+
+Email:
+driver.find_element(By.CSS_SELECTOR, "#ap_email[name='email']")
+
+Password:
+driver.find_element(By.CSS_SELECTOR, "#ap_password[name='password']")
+
+Password must be...:
+I am not able to find this element by css selector, can you please help?
+
+Re-enter password:
+driver.find_element(By.CSS_SELECTOR, "#ap_password_check")
+
+Continue button:
+driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
+
+Conditions:
+driver.find_element(By.CSS_SELECTOR, "[href*='register_notification_condition_of_use']")
+
+Privacy:
+driver.find_element(By.CSS_SELECTOR, "[href*='register_notification_privacy_notice']")
+
+Sign in:
+driver.find_element(By.CSS_SELECTOR, "[href*='/ap/signin?openid.pape']")
+"""

280292eb32f21d910c77d151f6d385bd58c41245
saghif <saghi.farhandi@gmail.com>
2022-05-24 21:31:56 -0700

    HW6-#2

--- features/steps/bestseller_page_steps.py
+++ features/steps/bestseller_page_steps.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then, step
-
+from selenium.webdriver.support import expected_conditions as EC
 
 SUBHEADER_LINK = (By.CSS_SELECTOR, '#zg_header a')
+EACH_LINK = (By.CSS_SELECTOR, "a[href*='ref=zg_bsnr_tab']")
 
 
 @given('Open Amazon BestSellers page')
@@ -14,3 +15,15 @@ def open_amazon_bestseller(context):
 def verify_subheader_links_count(context, expected_count):
     subheader_links = context.driver.find_elements(*SUBHEADER_LINK)  # [Webelement1, Webelement2, ..]
     assert len(subheader_links) == int(expected_count), f'Expected {expected_count} links, but got {len(expected_count)}'
+
+
+@then('Clicks on each top link and verify that correct page opens')
+def click_link(context):
+    all_links = context.driver.find_elements(*EACH_LINK)
+    for links in all_links:
+        links.find_elements(*EACH_LINK).click()
+        context.driver.wait.until(EC.url_contains('ref=zg_bs_tab'), 'Page did not open')
+
+
+
+
--- features/tests/bestseller_page.feature
+++ features/tests/bestseller_page.feature
@@ -4,3 +4,10 @@ Feature: Tests for Best Seller page
   Scenario: User sees correct number of subheader links
     Given Open Amazon BestSellers page
     Then Verify there are 5 subheader links
+
+  Scenario: Verify Best Sellers links opens correct page
+    Given Open Amazon page
+    Given Open Amazon BestSellers page
+    Then Clicks on each top link and verify that correct page opens
+
+
